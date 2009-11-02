282d78fced6fed93e2b2c9628e35bdfd5746c4bc
dnolen <david.nolen@gmail.com>
2009-11-02 01:37:42 -0500

    More examples.

--- DataStructures/DataStructures.m
+++ DataStructures/DataStructures.m
@@ -4,11 +4,15 @@ int main (int argc, const char * argv[]) {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
 	// ====================
-	// Dictinaries
+	// Dictionaries
 	// ====================
-	
-	NSDictionary *dict1 = [NSDictionary dictionaryWithObjectsAndKeys:@"foo", @"bar", nil];
-	NSMutableDictionary *dict2 = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"foo", @"bar", nil];
+
+	NSLog(@"----------------------");
+	NSLog(@"Dictionaries");
+	NSLog(@"----------------------");
+
+	NSDictionary *dict1 = [NSDictionary dictionaryWithObjectsAndKeys:@"bar", @"foo", nil];
+	NSMutableDictionary *dict2 = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"bar", @"foo", nil];
 	
 	@try {
 		[dict1 setValue:@"baz" forKey:@"foo"];
@@ -25,6 +29,10 @@ int main (int argc, const char * argv[]) {
 	// Arrays
 	// ====================
 	
+	NSLog(@"----------------------");
+	NSLog(@"Arrays");
+	NSLog(@"----------------------");
+
 	NSArray *ary1 = [NSArray arrayWithObjects:@"item1", [NSDate date], dict2, nil];
 	NSMutableArray *ary2 = [NSMutableArray arrayWithObjects:@"item1", [NSDate date], dict2, nil];
 	
@@ -38,22 +46,72 @@ int main (int argc, const char * argv[]) {
 	[ary2 removeObjectAtIndex:0];
 	
 	// ====================
+	// Sets
+	// ====================
+	
+	NSLog(@"----------------------");
+	NSLog(@"Sets");
+	NSLog(@"----------------------");
+	
+	NSArray *ary5 = [NSArray arrayWithObjects:@"bird", @"dog", @"cat", @"bird", @"zebra", @"lion", nil];
+	NSArray *ary6 = [NSArray arrayWithObjects:@"bird", @"dog", @"cat", @"bird", @"zebra", @"lion", nil];
+	NSSet *set1 = [NSSet setWithArray:ary5];
+	
+	NSLog(@"%@", set1);
+	
+	// ====================
+	// Equality
+	// ====================
+	
+	NSLog(@"----------------------");
+	NSLog(@"Equality");
+	NSLog(@"----------------------");
+
+	// BOOL Objective-C type
+	// YES == 1
+	// NO == 0
+	NSLog(@"ary5 == ary6 %d", [ary5 isEqual:ary6]);
+		
+	// ====================
+	// Arbitrary Keys
+	// ====================
+
+	NSLog(@"----------------------");
+	NSLog(@"Arbitrary Keys");
+	NSLog(@"----------------------");
+	
+	NSDictionary *dict3 = [NSDictionary dictionaryWithObjectsAndKeys:@"complex key", set1, nil];
+	NSSet *set2 = [NSSet setWithObjects:@"lion", @"dog", @"cat", @"bird", @"bird", @"zebra", nil];
+
+	NSLog(@"%@", [dict3 objectForKey:set2]);
+	
+	// ====================
 	// Iteration
 	// ====================
+
+	NSLog(@"----------------------");
+	NSLog(@"Iteration");
+	NSLog(@"----------------------");
 	
 	NSArray *ary3 = [NSArray arrayWithObjects:@"one", @"two", @"three", @"four", @"five", @"size", @"seven", @"eight", @"nine", @"ten", nil];
 	
 	// old school
 	for(int i = 0; i < [ary3 count]; i++) {
-		NSLog(@"%@", [ary3 objectAtIndex:i]);
+		//NSLog(@"%@", [ary3 objectAtIndex:i]);
 	}
 	
 	// new school
 	for(NSString *str in ary3) {
-		NSLog(@"%@", str);
+		//NSLog(@"%@", str);
 	}
 	
-	
+	// dictionary enumeration
+	NSArray *keys = [NSArray arrayWithObjects:@"first", @"middle", @"last", nil];
+	NSArray *values = [NSArray arrayWithObjects:@"Bob", @"Howard", @"Smith", nil];
+	NSDictionary *dict4 = [NSDictionary dictionaryWithObjects:values forKeys:keys];
+	for(id key in dict4) {
+		NSLog(@"%@ = %@", key, [dict4 objectForKey:key]);
+	}
 	
 	[pool drain];
 	return 0;
